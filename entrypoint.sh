cdf742ddb5f708d3299793b548d890a60360af76
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-01-22 13:22:14 -0700

    chore: Hack modification for deploying entire repository to another.

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,22 +92,24 @@ git config --global http.version HTTP/1.1
 	exit 1
 
 }
-ls -la "$CLONE_DIR"
 
 TEMP_DIR=$(mktemp -d)
+TEMP_DIR2=$(mktemp -d)
 # This mv has been the easier way to be able to remove files that were there
 # but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
 # including "." and with the exception of ".git/"
 mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
 
 # $TARGET_DIRECTORY is '' by default
-ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
+# ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
 
-echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
-rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
+echo "[+] Deleting $CLONE_DIR/*"
+mv "$CLONE_DIR"/* "$CLONE_DIR"/.git* "$TEMP_DIR2"
+echo "[+] Listing Contents after delete"
+ls -la "$CLONE_DIR"
 
-echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
-mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
+# echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
+# mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
 
 echo "[+] Listing Current Directory Location"
 ls -al
@@ -136,7 +138,8 @@ then
 fi
 
 echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
+rm -rf "$SOURCE_DIRECTORY"/.git
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/"
 cd "$CLONE_DIR"
 
 echo "[+] Files that will be pushed"
